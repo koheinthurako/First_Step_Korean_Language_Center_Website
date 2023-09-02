d96a1a260a8f513ca966aedb7fa62895b3c06312
koheinthurako <captainthura99@gmail.com>
2023-09-02 21:22:51 +0630

    change scrolling with webapi scrollIntoView

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ import './node_modules/bootstrap/dist/js/bootstrap.bundle';
 import ScrollReveal from 'scrollreveal';
 import './node_modules/waypoints/lib/noframework.waypoints';
 
+const course = document.querySelector(".navbar #course");
+const feature = document.querySelector(".navbar #feature");
+const contact = document.querySelector(".navbar #contact");
+const login = document.querySelector(".navbar #login");
 
+course.addEventListener('click', () => {
+    document.querySelector(".container #courses").scrollIntoView({behavior: "smooth"});
+});
+
+feature.addEventListener('click', () => {
+    document.querySelector(".container #features").scrollIntoView({behavior: "smooth"});
+});
+
+login.addEventListener('click', () => {
+    document.querySelector(".container #loginBox").scrollIntoView({behavior: "smooth"});
+});
+
+contact.addEventListener('click', () => {
+    document.querySelector("footer#contactBox").scrollIntoView({behavior: "smooth"});
+});
 
 let toDown = {
     distance: '50px',
